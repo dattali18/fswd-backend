51f850ea61e89c903addf0ad65b06f9f14f419ff
Daniel Attali <danielattali16@gmail.com>
2024-07-17 12:13:36 +0300

    tested first reponse

--- routes/articles.js
+++ routes/articles.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const marked = require("marked");
+const {marked} = require("marked");
+const { log } = require("console");
 const router = express.Router();
 
 router.get("/articles/:filename", (req, res) => {
   const filename = req.params.filename;
-  const filepath = path.join(__dirname, "articles", filename);
+  // the path to the file
+  // go up one level
+  const parentDir = path.join(__dirname, "..");
+  const filepath = path.join(parentDir, "articles", filename);
+
+  log(filepath);
 
   fs.readFile(filepath, "utf8", (err, data) => {
     if (err) {

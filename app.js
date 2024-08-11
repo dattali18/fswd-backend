e89915c140043e37e4725395e64cddd58feddfc9
Daniel Attali <danielattali16@gmail.com>
2024-08-11 10:37:50 +0300

    added error handling

--- app.js
+++ app.js
@@ -1,66 +1,61 @@
-import createError from 'http-errors';
-import express, { Router, json, urlencoded } from 'express';
-import cors from 'cors';
-import { join } from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from "http-errors";
+import express, { Router, json, urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-// MARK: connect to the database (MongoDB)
+// MARK: connecting MongoDB
 import connectDB from "./articles/connection.js";
 
 connectDB();
 
-// MARK: Router
-
+// MARK: Routers
 let apiRouter = Router();
 
-import usersRouter from './routes/users.js';
-import authRouter from './routes/auth.js';
-import articlesRouter from './routes/articles.js';
-import likesRouter from './routes/likes.js';
-import commentsRouter from './routes/comments.js';
+import usersRouter from "./routes/users.js";
+import authRouter from "./routes/auth.js";
+import articlesRouter from "./routes/articles.js";
+import likesRouter from "./routes/likes.js";
+import commentsRouter from "./routes/comments.js";
 
 let app = express();
 
 let corsOptions = {
-  origin: "http://localhost:5173"
+  origin: "http://localhost:5173",
 };
 
 app.use(cors(corsOptions));
 
-// view engine setup
-// app.set('views', join(__dirname, 'views'));
-// app.set('view engine', 'pug');
+// Middlewares
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
 
-// app.use(logger('dev'));
-// app.use(json());
-// app.use(urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(static(join(__dirname, 'public')));
-
-apiRouter.use('/users', usersRouter);
+// API routes
+apiRouter.use("/users", usersRouter);
 apiRouter.use("/auth", authRouter);
 apiRouter.use("/articles", articlesRouter);
 apiRouter.use("/comments", commentsRouter);
 apiRouter.use("/likes", likesRouter);
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404, "Resource not found"));
 });
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Only provide error details in development
+  const errorDetails = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // Respond with JSON
+  res.status(err.status || 500).json({
+    message: err.message,
+    error: errorDetails,
+  });
 });
 
 export default app;
